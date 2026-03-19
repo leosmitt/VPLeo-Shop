a4a2bb7fd91b3d54ebc3f172d3f7ec6e7588bd58
Leo Smitt <64788702+leosmitt@users.noreply.github.com>
2026-03-20 01:06:55 +0630

    Create Logo.jsx

---
+++ components/Logo.jsx
@@ -0,0 +1,9 @@
+export default function Logo() {
+  return (
+    <span className="text-2xl tracking-tight select-none">
+      <span className="font-black text-white">VP</span>
+      <span className="font-black text-orange-400">Leo</span>
+      <span className="font-thin text-white">.Shop</span>
+    </span>
+  )
+}
